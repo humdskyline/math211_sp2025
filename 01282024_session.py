b4333febfd2d064d6361c3ddd37c4a41d6a65106
humdskyline <45829754+humdskyline@users.noreply.github.com>
2025-01-27 16:54:53 -0800

    Add files via upload

---
+++ 01282024_session.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### <a href="http://www.data8.org/sp20/python-reference.html"> Python Reference Link <a>
+# 
+# Run the cell below so that we can set our modules up
+# 
+
+# In[ ]:
+
+
+import numpy as np
+from datascience import *
+import math
+
+
+# In[ ]:
+
+
+link = "https://raw.githubusercontent.com/humdskyline/data/main/first_class_survey_s2025.csv"
+first_survey = Table.read_table(link)
+first_survey.show(5)
+
+
+# ### 1. Write the Python code to find:
+# <i>What is the average height of the class?</i>
+
+# In[ ]:
+
+
+
+
+
+# ### 2. Write the Python code to find:
+# the major of the person in the second row
+
+# In[ ]:
+
+
+
+
+
+# ### 3.  Now try finding the previous answer a different way using Python
+
+# In[ ]:
+
+
+
+
+
+# ### 4. Find the heights and favorite colors of students in this class
+
+# In[ ]:
+
+
+
+
+
+# ### 5. Find the students in this class that have more than two people in their households.
+
+# In[ ]:
+
+
+
+
+
+# ### 6. Find the heights and favorite colors of students in this class that have more than two people in their households.
+
+# In[ ]:
+
+
+
+
